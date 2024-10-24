9f572ae567f2e1d12e16f3e9d7ad3d264d967824
quangvinh10042003 <qvinhdx@gmail.com>
2024-10-24 17:56:56 +0700

    commit

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -6,18 +6,27 @@ class Order {
   final DateTime createdAt;
   final String addresss;
   final String phone;
+  final String name;
 
-  Order({required this.id, required this.userId, required this.totalPrice, required this.status, required this.createdAt, required this.addresss, required this.phone});
+  Order(
+      {required this.id,
+      required this.userId,
+      required this.totalPrice,
+      required this.status,
+      required this.createdAt,
+      required this.addresss,
+      required this.phone,
+      required this.name});
 
   factory Order.fromJson(Map<String, dynamic> json) {
     return Order(
-      id: json['id'],
-      userId: json['user_id'],
-      totalPrice: json['total_price'],
-      status: json['status'],
-      createdAt: DateTime.parse(json['created_at']),
-      addresss: json['address'],
-      phone: json['phone']
-    );
+        id: json['id'],
+        userId: json['user_id'],
+        totalPrice: json['total_price'],
+        status: json['status'],
+        createdAt: DateTime.parse(json['created_at']),
+        addresss: json['address'],
+        phone: json['phone'],
+        name: json['name']);
   }
 }
